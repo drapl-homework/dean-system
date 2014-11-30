a316995048cf74397a4410605ada970f393d6fcf
draplater <tsjzadmin@gmail.com>
2014-11-30 17:16:29 +0800

    school: 增加学校数据

---
+++ src/school.h
@@ -0,0 +1,32 @@
+
+struct student //学生数据项
+{
+	int id; //学号
+	char name[35]; //姓名
+};
+
+struct teacher //教师数据项
+{
+	int id; //编号
+	char name[35]; //姓名
+};
+
+struct course //课程数据项
+{
+	int id; //课程编号
+	char name[35]; //课程名
+	int credit; //学分
+};
+
+struct student_course //学生选课、成绩数据项
+{
+	int student_id; //学生学号
+	int course_id; //选课课程编号
+	int score; //分数
+};
+
+struct student_course //教师上课数据项
+{
+	int teacher_id; //教师编号
+	int course_id; //课程编号
+};
